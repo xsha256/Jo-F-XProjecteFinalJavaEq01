2e59c97e05aee05f9b83e5ddcc2c8829cdf2277c
M <43494720+xsha256@users.noreply.github.com>
2025-05-16 22:57:10 +0200

    desarrolar el frontend del login

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,5 +1,24 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class LoginController {
+	@FXML private ImageView loginimg;
+	private Image imatge;
+
 	
+//	public void initialize() {	
+//		
+//		try {
+//			imatge = new Image(new FileInputStream("./img/login.png"));
+//			loginimg.setImage(imatge);
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
