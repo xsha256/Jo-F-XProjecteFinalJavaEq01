0c729fce4359170ac14f9060505e5cc7b8c7c6ce
M <43494720+xsha256@users.noreply.github.com>
2025-05-17 19:01:35 +0200

    agregar labels con transaccion

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,24 +1,68 @@
 package application;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
-	@FXML private ImageView loginimg;
-	private Image imatge;
+public class LoginController implements Initializable {
+//	@FXML private ImageView loginimg;
+//	private Image imatge;
+
+	@FXML
+	private Label labelCorreu;
+	@FXML
+	private Label labelContrasenya;
+
+	@FXML
+	private TextField correutxt;
+	@FXML
+	private TextField contrasenyatxt;
+
+	private final String promptCorreu = "Correu electrònic";
+	private final String promptContrasenya = "Contrasenya";
 
 	
-//	public void initialize() {	
-//		
-//		try {
-//			imatge = new Image(new FileInputStream("./img/login.png"));
-//			loginimg.setImage(imatge);
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		correutxt.setPromptText(promptCorreu);
+		correutxt.focusedProperty().addListener((obs, oldVal, newVal) -> {
+			if (newVal) {
+				correutxt.setPromptText("");
+				labelCorreu.setVisible(true);
+			} else {
+				if (correutxt.getText().isEmpty()) {
+					correutxt.setPromptText(promptCorreu);
+					labelCorreu.setVisible(false);
+				}
+			}
+		});
+
+		contrasenyatxt.textProperty().addListener((obs, oldVal, newVal) -> {
+			labelContrasenya.setVisible(!newVal.isEmpty() || contrasenyatxt.isFocused());
+		});
+
+		contrasenyatxt.setPromptText(promptContrasenya);
+
+		contrasenyatxt.focusedProperty().addListener((obs, oldVal, newVal) -> {
+			if (newVal) {
+				contrasenyatxt.setPromptText("");
+				labelContrasenya.setVisible(true);
+			} else {
+				if (contrasenyatxt.getText().isEmpty()) {
+					contrasenyatxt.setPromptText(promptContrasenya);
+					labelContrasenya.setVisible(false);
+				}
+			}
+		});
+
+		contrasenyatxt.textProperty().addListener((obs, oldVal, newVal) -> {
+			labelContrasenya.setVisible(!newVal.isEmpty() || contrasenyatxt.isFocused());
+		});
+
+	}
 }
