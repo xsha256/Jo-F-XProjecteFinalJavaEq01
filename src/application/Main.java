f372ec61bf8cdbadb1715b642b3376fd0322129d
yordaw <yorradgeo@alu.edu.gva.es>
2025-05-19 13:47:03 +0200

    primer commit Yordan

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,24 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
 			HBox root = (HBox)FXMLLoader.load(getClass().getResource("Login.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			
+			primaryStage.setMaximized(true);//para ejecutar en pantalla completa
+
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
