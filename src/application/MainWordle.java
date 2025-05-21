06cd121bb9fbe951bd8619f51d47659a6cb79069
Helena Prats Revert <helenaprats16@gmail.com>
2025-05-21 13:43:44 +0200

    commit alert

--- src/application/MainWordle.java
+++ src/application/MainWordle.java
@@ -4,9 +4,11 @@ import java.awt.Panel;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 
 
@@ -14,8 +16,8 @@ public class MainWordle extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("wordleLogin.fxml"));
-			Scene scene = new Scene(root);
+			VBox escena = (VBox)FXMLLoader.load(getClass().getResource("wordleLogin.fxml"));
+			Scene scene = new Scene(escena);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setMaximized(true);
--- src/application/WordleController.java
+++ src/application/WordleController.java
@@ -2,16 +2,35 @@ package application;
 
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class WordleController implements Initializable {
 
@@ -19,12 +38,27 @@ public class WordleController implements Initializable {
 	@FXML private GridPane grid1; //tabla de paraules
 	@FXML private GridPane grid2;//teclat
 	
+	@FXML
+	private HBox escena;
 	
 	private TextField[][] caselles = new TextField[6][5];
-	private String paraula="ENVIO";
 	
+	private String paraula=paraulaAleatoria();
 	
-
+	public void enrereInici(ActionEvent e) {
+		try {
+			Parent escena = FXMLLoader.load(getClass().getResource("wordleLogin.fxml"));
+			Scene escena2 = new Scene(escena);
+			//obtenim la finestra de l'aplicació actual
+			 Stage window = (Stage) ((Node) e.getSource()).getScene().getWindow();			
+			 window.setScene(escena2);
+			 window.setTitle("Wordle");
+			 window.show();		
+		} catch (IOException error) {
+			error.printStackTrace();
+		}
+	}
+	
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		for (int fila = 0; fila < 6; fila++) {
 			for (int columna = 0; columna < 5; columna++) {
@@ -77,6 +111,8 @@ public class WordleController implements Initializable {
 							}
 	                    }
 				}});
+			
+			
 			}
 			
 		}
@@ -90,6 +126,7 @@ public class WordleController implements Initializable {
 		
 	}
 	public void comprovarFila(int fila) {
+		boolean encertada =true;
 		for (int columna = 0; columna < 5; columna++) {
 			TextField celda = caselles[fila][columna];
 			String text = celda.getText();//agafem la lletra del usuari
@@ -102,16 +139,71 @@ public class WordleController implements Initializable {
 		        } else if (paraula.contains(String.valueOf(lletraUsuari))) {
 		            // Groc: lletra està en la paraula però en altra posició
 		            celda.setStyle("-fx-background-color: #e4a81d; -fx-text-fill:white; -fx-font-size: 20px; -fx-font-weight: bold;");
+		            encertada =false;
 		        } else {
 		            // Gris: lletra no està en la paraula
 		            celda.setStyle("-fx-background-color: lightgrey; -fx-text-fill:white; -fx-font-size: 20px; -fx-font-weight: bold;");
+		            encertada =false;
 		        }
-				
-				
+				if ((fila==5 &&columna==4)&&!encertada) {//si estem en la fila 5 i no la ha acertat
+					
+					try {
+						
+						File fitxer=new File("icona.png");
+						Image iconaAlert;
+						iconaAlert = new Image(new FileInputStream(fitxer));
+						 ImageView alertView = new ImageView(iconaAlert);
+						 alertView.setFitWidth(100);
+						 alertView.setPreserveRatio(true);
+						Alert alert = new Alert(AlertType.NONE);
+						alert.setTitle("Resultat"); // text en la bara de la finestra
+						alert.setHeaderText("INCORRECTE"); // text al costat de la icona
+						alert.setContentText("No has adivinant la paraula.\n La paraula correcta es: "+paraula.toUpperCase()); // missatge
+						alert.setResizable(true); // es pot canviar la mida o no?
+						alert.getDialogPane().setPrefSize(300, 300); // mida del diàleg
+						alert.getDialogPane().getButtonTypes().add(ButtonType.OK);
+						 alert.setGraphic(alertView);
+
+						alert.showAndWait(); 
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
 				
 			}
 			
+			
 				
+	}
+	
+	
+	public String paraulaAleatoria() {
+		Random aleatori = new Random();
+		String paraulaAleatoria="";
+		ArrayList<String> paraules=new ArrayList<String>();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("paraules.txt"));
+			String linea ="";
+			
+			while ((linea=reader.readLine())!=null) {
+				paraules.add(linea);
+			}
+			reader.close();
+			int index=aleatori.nextInt(0,200);
+			paraulaAleatoria=paraules.get(index);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return paraulaAleatoria;
+
+
+		
 	}
 			
 	
