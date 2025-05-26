74cca8356f32f39a550a7b5566a5a7e7c9a5ed9c
yordaw <yorradgeo@alu.edu.gva.es>
2025-05-26 12:51:03 +0200

    copia de reserva

---
+++ src/application/ConexionBBDD.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class  ConexionBBDD {
+	public static Connection conectar() {
+		
+		try {
+		Class.forName("org.mariadb.jdbc.Driver");
+		String urlBaseDades = "jdbc:mariadb://localhost:3306/jofx";
+		String user = "root";
+		String pwd = "";
+		Connection c = DriverManager.getConnection(urlBaseDades, user, pwd);
+		return c;
+		} catch (Exception e) {
+			System.out.println("Error conexion: "+ e);
+		}
+		return null;
+	}
+}
