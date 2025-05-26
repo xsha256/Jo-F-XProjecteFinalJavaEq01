bed73799d6900f55c4ddb9e2a0b270e798ead65e
yordaw <yorradgeo@alu.edu.gva.es>
2025-05-26 12:48:15 +0200

    push para tener como backup

--- src/application/MainMenu.java
+++ src/application/MainMenu.java
@@ -1,31 +1,30 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-
 public class MainMenu extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
-			HBox root = (HBox)FXMLLoader.load(getClass().getResource("Menu.fxml"));
+
+			HBox root = (HBox) FXMLLoader.load(getClass().getResource("Menu.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			
-			primaryStage.setTitle("Menu Principal Usuario");//define el titulo de la ventana 
-			primaryStage.setMaximized(true);//para ejecutar en pantalla completa
+
+			primaryStage.setTitle("Menu Principal Usuario");// define el titulo de la ventana
+			primaryStage.setMaximized(true);// para ejecutar en pantalla completa
 
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/application/MenuController.java
+++ src/application/MenuController.java
@@ -4,27 +4,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MenuController implements Initializable {
+	//atributos
+	private Connection c;	
+	private String emailUsuario;
 	
 	//Recogemos los botones "jugar" que tiene cada juego
 	@FXML private Button jugarPescamines;
@@ -34,23 +45,34 @@ public class MenuController implements Initializable {
 	
 	@FXML private HBox root;
 	
-	//id de la variable para el nombre del usuario en el titulo e Bie
+	//id de la variable para el nombre del usuario en el titulo
 	@FXML private Label tituloNomUsuari;
 	@FXML private ImageView imatgePerfilUsuari;
 	
+	//id de las imagenes de cada juego
+	@FXML private ImageView fotoPescamines;
+	@FXML private ImageView fotoWordle;
+	@FXML private ImageView fotoJocDeLaVida;
+	@FXML private ImageView fotoPixelArt;
+	
+	//items del MenuButton
+	@FXML private MenuItem itemBaixa;
+	@FXML private MenuItem itemLogout;
+	
+	
 	//metodo que hace que se inicie 
 	public void initialize(URL location, ResourceBundle resources) {
-		String emailUsuario=LoginController.EMAIL; //poner nombre archivo.nombreVariable del login de Moha;
+		this.emailUsuario=LoginController.EMAIL; //poner nombre archivo.nombreVariable del login de Moha;
 		
 		try {
 			// cargar el driver de MariaDB... con una vez sobra creo :) 
 	        Class.forName("org.mariadb.jdbc.Driver");
 	        
-			//Conexion BBDD--------------------------------------------------------------
-			String urlBaseDatos = "jdbc:mariadb://localhost:3306/jofx";
-			String usuario = "root";
-			String contra = "";
-			Connection c = DriverManager.getConnection(urlBaseDatos, usuario, contra);
+//			//Conexion BBDD--------------------------------------------------------------
+//			String urlBaseDatos = "jdbc:mariadb://localhost:3306/jofx";
+//			String usuario = "root";
+//			String contra = "";
+			this.c = ConexionBBDD.conectar();
 			//----------------------------------------------------------------------------
 			
 			//llamamos a la funcion que comprueba el nombre de usuario
@@ -61,11 +83,108 @@ public class MenuController implements Initializable {
 			//funcion para asignar la imagen de perfil de usuario al botón que la contiene
 			asignarImagenPerfilUsuario(c,emailUsuario,imatgePerfilUsuari);
 			
+			
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+				
+	}
+	
+	
+	//al pulsar esto, se borra la cuenta del usuario
+	public void actionBaja(ActionEvent e){
+		//al entrar aquí al usuario le sale el alert para confirmar
+	    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+	    alert.getDialogPane().setId("alertDarseDeBaja");
+	    alert.setTitle("🚩 Confirmació");
+	    alert.setHeaderText("Estàs segur que vols donar-te de baixa?");
+	    alert.setContentText("Aquesta acció eliminarà el teu usuari per sempre.");
+	    
+	    //botones personalizados
+	    ButtonType botAceptar = new ButtonType("Acceptar", ButtonBar.ButtonData.OK_DONE);
+	    ButtonType botCancelar = new ButtonType("Cancel·lar", ButtonBar.ButtonData.CANCEL_CLOSE);
+	    
+	    alert.getButtonTypes().setAll(botAceptar,botCancelar);//añadir los botones
+	    
+	    DialogPane dialogPane = alert.getDialogPane();
+
+	    //color texto
+		 Node header = dialogPane.lookup(".header-panel");
+		 header.setStyle("-fx-background-color: #e8e8e8;");
+
+		 Node content = dialogPane.lookup(".content");
+		 content.setStyle("-fx-text-fill: white;"+"-fx-background-color: #0d262e;");
+		 
+		 Node buttonBar = dialogPane.lookup(".button-bar");
+		 buttonBar.setStyle("-fx-background-color: #0d262e;");
+
+		 
+		 alert.getDialogPane().getStylesheets().add(
+				    getClass().getResource("application.css").toExternalForm()
+		);
+
+	    Button botonAceptar = (Button) alert.getDialogPane().lookupButton(botAceptar);
+	    botonAceptar.setStyle(
+	        "-fx-background-color: #2a7963;" +     
+	        "-fx-text-fill: #e8e8e8;" +           
+	        "-fx-font-weight: bold;"
+	    );
+	    botonAceptar.setCursor(Cursor.HAND);
+
+	    Button botonCancelar = (Button) alert.getDialogPane().lookupButton(botCancelar);
+	    botonCancelar.setStyle(
+	        "-fx-background-color: #f44336;" +     
+	        "-fx-text-fill: #e8e8e8;" +
+	        "-fx-font-weight: bold;"
+	    );
+	    botonCancelar.setCursor(Cursor.HAND);
+
+	    Optional<ButtonType> resultado = alert.showAndWait();
+
+	    if(resultado.isPresent() && resultado.get() == botAceptar) {//si pulsa ok -> adiós cuenta. Si pulsa no, pues nada
+	    	String sentencia = "DELETE FROM usuari WHERE email =?";
+
+		    try {
+		        PreparedStatement ps = c.prepareStatement(sentencia);
+		        ps.setString(1, emailUsuario);
+	
+		        int cant = ps.executeUpdate();
+		        if (cant>0) {
+		        	System.out.println("Usuari eliminat correctament.");
+		        	actionLogout(e);//llamamos de nuevo a Login ya que si se ha dado de baja, deberia salir
+		        } else {
+		            System.out.println("Usuari no eliminat correctament... ");
+		        }
+		    } catch (SQLException e1) {
+		        e1.printStackTrace();
+		    }
+	    }else {
+		    System.out.println("Borrat Cancelat");
+	    }
+	    
+
+	}
+	
+	//metodo para cerrar sesión
+	public void actionLogout(ActionEvent e) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene escena= new Scene(root);
+			Stage window = new Stage();
+			window.setScene(escena);
+			window.setTitle("Login");
+			window.show();
+			
+			//cerrar la ventana actual
+			Stage ventanaActual = (Stage) itemLogout.getParentPopup().getOwnerWindow();
+	        ventanaActual.close();
+	        
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		
 	}
+
 	
 	public String ponerNombreUsuarioBD(Connection c, String emailUsuari) {
 	    String sentencia = "SELECT nom FROM usuari WHERE email = ?;";
@@ -102,6 +221,20 @@ public class MenuController implements Initializable {
 	            if (input != null) {
 	                Image imagen = new Image(input);
 	                imagenPerfil.setImage(imagen);//añade la imagen directamente
+	                //imagenPerfil.setFitWidth(110);
+	                //imagenPerfil.setFitHeight(110);
+	                
+	                //------------------------------------------------
+	                imagenPerfil.setImage(imagen);                                                                                    
+	                imagenPerfil.setFitWidth(65);
+	                imagenPerfil.setFitHeight(65);
+	                imagenPerfil.setPreserveRatio(false);
+	                imagenPerfil.setSmooth(true);
+	        		double radius = imagenPerfil.getFitWidth() / 2;
+	        		Circle clip = new Circle(radius, radius, radius);
+	        		imagenPerfil.setClip(clip);
+	        		//-----------------------------------------------------
+	        		
 	            }else {
 	            	System.out.println("Imatge no trobada! ");
 	            }
@@ -115,13 +248,13 @@ public class MenuController implements Initializable {
 	//intento de "reciclar" codigo para los botones del menu-----------------------------
 	private void abrirVentanaJuego(String rutaFXML, String tituloVentana, ActionEvent e) {
 	    try {
-	        HBox root = FXMLLoader.load(getClass().getResource(rutaFXML));
-	        Scene scene = new Scene(root);
+	        Parent root = FXMLLoader.load(getClass().getResource(rutaFXML));
+	        Scene scene = new Scene(root,600,500);//ponemos la medida ya que es una ventana con poca información anchoXalto
 	        Stage window = new Stage();
 	        window.setScene(scene);
 	        window.setTitle(tituloVentana);
-	        window.setMaximized(true);
-
+//	        window.setMaximized(true);
+	        
 	        window.initModality(Modality.WINDOW_MODAL);
 	        Stage menuStage = (Stage) ((Node) e.getSource()).getScene().getWindow();
 	        window.initOwner(menuStage);
@@ -146,7 +279,7 @@ public class MenuController implements Initializable {
 	}
 
 	public void actionJocDeLaVida(ActionEvent e) {
-	    abrirVentanaJuego("jocDeLaVida.fxml", "Joc de la Vida", e);
+	    abrirVentanaJuego("Dificultad.fxml", "Joc de la Vida", e);
 	}
 
 	
